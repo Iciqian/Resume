10ec10db94ad27ce698984d975dad7b4923f0f48
iciqian <iciqian@163.com>
2017-06-19 02:23:50 +0800

    1.0

---
+++ js/resume.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        centeredSlides: true,
+        keyboardControl: true,
+        paginationClickable: true,
+        spaceBetween: 30,
+        loop: true
+    });
+
