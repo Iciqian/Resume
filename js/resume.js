0fdf106b315f6578f44e2dcefb177c48db778fc3
iciqian <iciqian@163.com>
2017-06-22 22:45:50 +0800

    改版

--- js/resume.js
+++ js/resume.js
@@ -9,3 +9,15 @@ var swiper = new Swiper('.swiper-container', {
         loop: true
     });
 
+$('#container').fullpage({
+				anchors: ['anchor1','anchor2','anchor3','anchor4','anchor5','anchor6'],
+				menu: '#menu',
+				afterLoad: function(anchorLink, index){
+					if (index == 1) {
+						$('#menu').fadeOut();
+					}else{
+						$('#menu').fadeIn();
+					}
+					
+				}
+			})
